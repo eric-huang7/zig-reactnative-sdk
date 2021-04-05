09861de59e7213bedc96a60994c9e4bde54701ee
Eric Huang <erichuang676@gmail.com>
2021-04-05 19:10:07 +0300

    Added missing methods

--- ios/RCTZCameraModule.m
+++ ios/RCTZCameraModule.m
@@ -6,6 +6,21 @@
 
 RCT_EXPORT_MODULE();
 
+static ZiggeoRecorder2 *lastZiggeoRecorder;
+
+RCT_EXPORT_METHOD(startRecording:(NSString *)path maxDuration:(int)maxDuration) {
+    if (lastZiggeoRecorder != nil) {
+        lastZiggeoRecorder.maxRecordedDurationSeconds = (double)maxDuration;
+        [lastZiggeoRecorder startRecordingToFile:path];
+    }
+}
+
+RCT_EXPORT_METHOD(stopRecording) {
+    if (lastZiggeoRecorder != nil) {
+        [lastZiggeoRecorder stopRecording];
+    }
+}
+
 - (NSArray<NSString *> *)supportedEvents
 {
     return @[
